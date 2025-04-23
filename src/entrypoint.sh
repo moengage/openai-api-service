4252259ff96db2678f8544db480b59751b373d54
manoj-me <manoj@moengage.com>
2025-04-23 09:10:40 +0530

    [SRE-8128]: pull from parameter store

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+PARAMETER_NAME="BEDROCK_CERTIFICATE"
+parameter_value=$(aws ssm get-parameter --name $PARAMETER_NAME --with-decryption --query Parameter.Value --output text)
 
 cafile=/usr/local/lib/python3.12/site-packages/certifi/cacert.pem
 
-echo $BEDROCK_CERTIFICATE >> $cafile
+if [ -z "$parameter_value" ]; then
+  echo "Failed to retrieve parameter value from AWS Parameter Store"
+  exit 1
+fi
+
+echo "$parameter_value" > $cafile
+
+echo "Content retrieved from AWS Parameter Store and written to file successfully"
 
 uvicorn api.app:app --host 0.0.0.0
